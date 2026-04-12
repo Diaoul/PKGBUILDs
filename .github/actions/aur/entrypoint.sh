414f3dd5ed6935986ca4c93048c5cb0b7921e91d
Antoine Bertin <antoine@bertin.io>
2026-04-12 15:16:25 +0200

    fix(aur-action): don't upgrade system when installing deps with paru

--- .github/actions/aur/entrypoint.sh
+++ .github/actions/aur/entrypoint.sh
@@ -20,7 +20,7 @@ echo "::endgroup::"
 
 echo "::group::Installing depends using paru"
 source PKGBUILD
-paru -Syu --removemake --needed --noconfirm "${depends[@]}" "${makedepends[@]}"
+paru -S --removemake --needed --noconfirm "${depends[@]}" "${makedepends[@]}"
 echo "::endgroup::"
 
 echo "::group::Running makepkg"
